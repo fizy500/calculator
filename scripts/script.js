d0d2250c7b3ceadba9b8a3d79cffda57b73562d3
Otuekong idongesit <otuekongarthur@gmail.com>
2022-06-27 15:19:13 +0100

    Added new functionality: Calculator now only accepts numbers.

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,21 @@ const firstNumber = prompt("Please input first Number");
 let sign = prompt('+,-,*,/');
 const secondNumber = prompt("Please input second Number");
 const placeholder = 'The answer is';
+const placeholder2 = 'Please use a number';
+const signValue = sign.valueOf();
 
-if (sign == '+') {
-    alert(placeholder.concat(' ',Number(firstNumber) + Number(secondNumber) ));
+if (isNaN(firstNumber) == false && isNaN(secondNumber) == false && sign === '+') {
+    alert(placeholder.concat(' ', Number(firstNumber) + Number(secondNumber)));
 }
-else if (sign === '-') {
+else if (isNaN(firstNumber) == false && isNaN(secondNumber) == false && sign === '-') {
     alert(placeholder.concat(' ', firstNumber - secondNumber));
 }
-else if (sign === '*') {
+else if (isNaN(firstNumber) == false && isNaN(secondNumber) == false && sign === '*') {
     alert(placeholder.concat(' ', firstNumber * secondNumber));
 }
-else if (sign === '/') {
+else if (isNaN(firstNumber) == false && isNaN(secondNumber) == false && sign === '/') {
     alert(placeholder.concat(' ', firstNumber / secondNumber));
+}
+else if (isNaN(firstNumber) == true && isNaN(secondNumber) == true && signValue == sign) {
+    alert(placeholder2);
 }
